e4ee122a8eae1d12fb6e1f0a34e1c9ef314d482a
coder-Rit <001patilritesh@gmail.com>
2024-09-13 20:01:56 +0530

    fix: add redis username and password

--- config/redisClient.js
+++ config/redisClient.js
@@ -3,6 +3,8 @@
 const redisClient = {
   host: process.env.REDIS_URI, // Replace with your Redis server hostname or IP
   port: process.env.REDIS_PORT, // Replace with your Redis server port if needed
+  username: process.env.REDIS_USER,  // Redis username (if required, for Redis 6+)
+  password: process.env.REDIS_PASSWORD,  // Redis password
 }
 
 export default redisClient;
